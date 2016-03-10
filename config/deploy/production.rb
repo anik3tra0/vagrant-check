e8e044d5658234f63a97edaa373d9ca9e2d4e24b
anik3tra0 <anik3tra0@gmail.com>
2016-03-10 16:14:31 +0530

    Capistrano Deploy Settings

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
+set :stage, :production
+server '188.166.224.230', user: 'deploy', roles: %w{web app db}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
